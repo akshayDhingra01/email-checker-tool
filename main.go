40fdea84a6c1bdb15759dc3c4a1162668cbd71c0
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-21 14:53:43 +0530

    ADD: Main func and logic

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-
+	"bufio"
+	"net"
+	"os"
+	"fmt"
+	"log"
+	"strings"
 )
 
 func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMx, hasSPF, sprRecord, hasDMARC, dmarcRecord\n" )
+
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string){
 
 }
