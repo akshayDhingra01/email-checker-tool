6095a9a1975a48e3650a2544415a7b244794e741
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-21 15:08:01 +0530

    ADD: check email logic

--- main.go
+++ main.go
@@ -23,5 +23,50 @@ func main(){
 }
 
 func checkDomain(domain string){
+	var hasMx, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil{
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMx = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil{
+		log.Printf("Error: %v\n", err)
+	}
+
+	fmt.Println(txtRecords)
+
+	for _, record := range(txtRecords){
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+
+	
+	if err != nil{
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range(dmarcRecords){
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v\n" , domain, hasMx, hasSPF, spfRecord, hasDMARC, dmarcRecord )
 
 }
